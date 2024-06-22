add3d1d9b91b50272ae060c70c067d15bbf80b35
Myth <mudnoethankgod@gmail.com>
2024-06-22 15:12:11 +0100

    project1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+product = number1 * number2
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", subtract)
+print("Multiplication of 10 and 5 is", product)
