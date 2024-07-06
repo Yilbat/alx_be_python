d7c0c845026b24661d50cd14afeb266dc830ca35
Myth <mudnoethankgod@gmail.com>
2024-07-06 16:49:49 +0100

    added corrections

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = float(input("Enter a number to see its multiplication table:"))
+number = float(input("Enter a number to see its multiplication table: "))
 
 for num in range(1,11):
     product = number * num
