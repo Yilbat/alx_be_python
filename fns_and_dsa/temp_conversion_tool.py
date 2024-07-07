50637841e19ac4da04e6bf02265c35a52a491643
Myth <mudnoethankgod@gmail.com>
2024-07-07 23:25:58 +0100

    m

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,12 +5,6 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 def convert_to_celsius(fahrenheit):
   """Converts a temperature from Fahrenheit to Celsius.
-
-  Args:
-      fahrenheit: The temperature in Fahrenheit (float).
-
-  Returns:
-      The temperature converted to Celsius (float).
   """
   celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
   return celsius
@@ -18,12 +12,6 @@ def convert_to_celsius(fahrenheit):
 
 def convert_to_fahrenheit(celsius):
   """Converts a temperature from Celsius to Fahrenheit.
-
-  Args:
-      celsius: The temperature in Celsius (float).
-
-  Returns:
-      The temperature converted to Fahrenheit (float).
   """
   fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
   return fahrenheit
