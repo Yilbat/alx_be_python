c52f1519cf54ea518df31c4c9d6cc2e935211a8b
Myth <mudnoethankgod@gmail.com>
2024-07-14 17:01:42 +0100

    r

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,9 +9,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2,3),5)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,3),5)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,3),5)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(2,3),5)
         self.assertEqual(self.calc.divide(2,0),ZeroDivisionError)
 
