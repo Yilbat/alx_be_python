4101b440c7817cc6de6d32d2b997a1688a594bf9
Myth <mudnoethankgod@gmail.com>
2024-07-14 16:14:38 +0100

    mm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,11 +17,3 @@ class BankAccount:
         current_balance = self.account_balance
         print(f"Current Balance: ${current_balance:.2f}")
 
-
-TG = BankAccount(20000)
-
-print(TG.display_balance())
-# TG.deposit(2000)
-# print(TG.display_balance())
-# TG.withdraw(20000)
-# print(TG.display_balance())
