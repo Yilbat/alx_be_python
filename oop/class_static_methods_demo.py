05877eddd0a50cc026b6218694ac734559b9fdc6
Myth <mudnoethankgod@gmail.com>
2024-07-21 19:40:47 +0100

    oop

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+# class_static_methods_demo.py
+
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        return a + b
+
+    @classmethod
+    def multiply(cls, a: float, b: float) -> float:
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
